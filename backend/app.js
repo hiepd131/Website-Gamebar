c14d467044520e323af14cb14473bc22b74849c1
Hiep Duong <duonghiep131@gmail.com>
2023-04-13 15:49:43 +0700

    fix: update static root in app.js

--- backend/app.js
+++ backend/app.js
@@ -48,12 +48,11 @@ app.use('/api/v1', auth)
 app.use('/api/v1', payment)
 app.use('/api/v1', order)
 
-const root = path.join(__dirname,'../frontend','build')
 if (process.env.NODE_ENV === 'PRODUCTION') {
-    app.use(express.static(root))
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
 
     app.get('*', (req, res) => {
-        res.sendFile('index.html',{root})
+        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
     })
 }
 
